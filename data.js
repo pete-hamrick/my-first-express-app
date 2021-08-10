1f12115b5265967f91e0a8ccb721f42f66fdedf0
Pete Hamrick <pete.hamrick@gmail.com>
2021-08-10 15:35:52 -0700

    updated data.js with more information

--- data.js
+++ data.js
@@ -2,18 +2,30 @@ const discData = [
     {
         id: 1,
         disc: 'Aviar',
+        type: 'Putt/Approach',
+        stable: true,
+        plastics: 'star, gstar, champion, xt, r-pro, glow, dx'
     },
     {
         id: 2,
         disc: 'Roc',
+        type: 'Midrange',
+        stable: true,
+        plastics: 'star, pro, glow, dx'
     },
     {
         id: 3,
         disc: 'Essence',
+        type: 'Fairway Driver',
+        stable: false,
+        plastics: 'neo, geo'
     },
     {
         id: 4,
         disc: 'Beast',
+        type: 'Distance Driver',
+        stable: true,
+        plastics: 'star, gstar, champion, blizzard, pro, glow, dx'
     },
 ];
 module.exports = discData;
